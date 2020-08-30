574e89c48d286b6420a1f972073ff8598578a3f7
WalkerLauryGit <walkerclaury@protonmail.com>
2020-08-29 21:28:24 -0500

    All event listeners working

--- src/components/Base.js
+++ src/components/Base.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Keys from './Keys'
 import Controls from './Controls'
 const Base = () => {
+
+  
+
   return (
     <div className="container" id="drum-machine">
       <Keys/>
--- src/components/Key.js
+++ src/components/Key.js
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+
+
 
 export default function Key(props) {
 
-  function changeBackgroundOnMouseDown(e){
+
+  const handleKeyPress = e => {
+    let audio = document.getElementById(e.key)
+    let sound = new Audio(audio.src)
+    audio.parentElement.style.backgroundColor = 'red'
+    sound.play()
+    setTimeout(function(){
+      audio.parentElement.style.backgroundColor = 'white'
+    }, 100)
     
-    
-    e.target.style.backgroundColor = 'red'
   }
 
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress)
+    
+    return () =>{
+      document.removeEventListener('keydown')
+    }
+  },[])
+
+  // function handleKeyPress(e){
+  //   console.log(e)
+  // }
+
+  function changeBackgroundOnMouseDown(e){
+    let audio = e.target.querySelector('audio').src;
+    let sound = new Audio(audio)
+    sound.play()
+    e.target.style.backgroundColor = 'red'
+  }
+  
   function changeColorToWhiteMouseUp(e){
     e.target.style.backgroundColor = "white"
   }
 
-
   return (
     <div className="drum-pad" onMouseDown={changeBackgroundOnMouseDown} onMouseUp={changeColorToWhiteMouseUp}>
-      <p>{props.letter}</p>
-      <audio src={props.audio}></audio>
+      {props.letter}
+      <audio src={props.audio} className="clip" id={props.letter}></audio>
     </div>
   )
 }
+
+
+  
+
