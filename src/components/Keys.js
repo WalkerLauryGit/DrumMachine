133354c7cf6ee58d22494720aeac218cf02c1369
WalkerLauryGit <walkerclaury@protonmail.com>
2020-08-29 21:38:33 -0500

    added more sounds

--- src/components/Keys.js
+++ src/components/Keys.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Key from './Key'
 const Keys = () => {
   const keys = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c' ]
-  const audio = ['https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3", "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3", "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3","https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3","https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3","https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3","https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3","https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"]
+  const audio = ['https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3", "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3", "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3","https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3","https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3","https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3","https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3","https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"]
 
 
   return (
